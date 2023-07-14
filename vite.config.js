c7051ccae3c761220bebdbde2a91f2e60f79a6ec
Sebastian <sebastian.buettner@coremedia.com>
2023-07-14 16:13:27 +0200

    Updating config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 // vite.config.js
-export default {
-  server: {
-    port: 3000
+import { defineConfig } from "vite";
+
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  if (command === 'build') {
+    return {
+      base: '/coremedia-studio-preview-integration-example/'
+    }
+  } else {
+    return {
+      server: {
+        port: 3000
+      },
+    }
   }
-}
+})
